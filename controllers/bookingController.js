a2d87760d857b2901307a3efc9b9289418d83da9
sypanchal1810 <saurabh.panchal1810@gmail.com>
2023-03-04 04:50:52 +0530

    Stripe Webhook Implemented

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -9,6 +9,8 @@ const catchAsync = require('./../utils/catchAsync');
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // Get the currently booked tour
   const tour = await Tour.findById(req.params.tourId);
+  
+  if (!tour) return next(new AppError('Tour not found!', 404));
 
   // Crate checkout session
   const session = await stripe.checkout.sessions.create({
@@ -47,9 +49,10 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = catchAsync(async session => {
   console.log(session);
   const tour = session.client_reference_id;
-  const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items.price_data.unit_amount / 100;
-
+  const user = (await User.findOne({ email: session.customer_email }))._id;
+  const price = session.amount_total / 100;
+  
+  console.log(tour, user, price);
   await Booking.create({ tour, user, price });
 });
 
